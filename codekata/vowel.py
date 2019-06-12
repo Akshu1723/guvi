9fb281812ceb7d7ec282dd5c691b5cebd713a3bc
Akshu1723 <51662199+Akshu1723@users.noreply.github.com>
2019-06-12 12:16:10 +0530

    Create vowel.py

---
+++ codekata/vowel.py
@@ -0,0 +1,8 @@
+k=input()
+if k.isalpha():
+  if(k=='a' or k=='e' or k=='i' or k=='o' or k=='u' or k=='A' or k=='E' or k=='I' or k=='O' or k=='U'):
+    print ("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
