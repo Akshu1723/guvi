79c172c78d4ea838e2b95a0576cf9184a31d572c
Akshu1723 <51662199+Akshu1723@users.noreply.github.com>
2019-06-14 09:45:57 +0530

    Update fibanocci.py

--- codekata/fibanocci.py
+++ codekata/fibanocci.py
@@ -1,10 +1,10 @@
-nk=int(input())
+nks=int(input())
 aks=1
 bks=1
 print(aks,bks,end=" ")
-while(nk-2):
+while(nks-2):
     ck=aks+bks
     aks=bks
     bks=ck
     print(ck,end=" ")
-    nk=nk-1
+    nks=nks-1
